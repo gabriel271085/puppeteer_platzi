0e293bff0a6c283f37d6b94771b8d38466d5da88
Gabriel Cayoja <gabriel271085@gmail.com>
2023-07-20 00:03:24 -0400

    elementos y funciones

---
+++ __tests__/waitingTypes.test.js
@@ -0,0 +1,32 @@
+const puppeteer = require('puppeteer')
+
+describe ('waiting types',()=>{
+
+    it('show the waiting types', async ()=>{
+        const browser = await puppeteer.launch({
+            headless: false,
+            defaultViewport: null
+        })
+        const page = await browser.newPage()
+        //waituntil realiza la funcion de esperar hasta que no haya mas movimiento de red
+        await page.goto('https://demo.guru99.com/test/simple_context_menu.html', {waitUntil: 'networkidle0'})
+
+        //espera explicita con la nueva sintaxis
+        await new Promise(r => setTimeout(r, 2000))
+
+        //espera por un selector CSS y verificar que este visible
+
+        await page.waitForSelector('#authentication > span', {visible: 'true'})
+        
+        //esperar por un XPATH
+
+        await page.waitForXPath('//img[@src="img/logo.png"]')
+
+        //espera por funcion
+   
+        
+
+
+        
+    },35000)
+})
